64fabf5e5c12a20a66e735bbd4cb15b9482c9a02
MurungaOwen <owenhood80@gmail.com>
2023-04-03 15:43:01 +0300

    added my js lesson 1 file

---
+++ 1.js
@@ -0,0 +1,8 @@
+class students{
+    constructor(name,course){
+        this.name=name;
+        this.course=course;
+    }
+}
+student1=new students("hood","Computer science");
+console.log(student1.forEach(key,Element))
